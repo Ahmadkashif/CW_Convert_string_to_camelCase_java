9e17ff8d24aaef8817f233fa66b5d8e50e45f12a
Ahmadkashif <ahmadkashif983@gmail.com>
2020-10-08 21:47:23 +0500

    initial commit

---
+++ src/MainClass.java
@@ -0,0 +1,12 @@
+import java.lang.StringBuilder;
+
+public class MainClass {
+
+    static String toCamelCase(String s){
+        return "";
+    }
+    public static void main(String[] args){
+
+        return ;
+    }
+}
