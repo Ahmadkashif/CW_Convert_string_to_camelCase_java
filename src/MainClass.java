61a3a38f0bf6abf3e73cb4fac912c09bad1f481c
Ahmadkashif <ahmadkashif983@gmail.com>
2020-10-09 13:24:45 +0500

    e

--- src/MainClass.java
+++ src/MainClass.java
@@ -3,6 +3,9 @@ import java.lang.StringBuilder;
 public class MainClass {
 
     static String toCamelCase(String s){
+        for (int i = 0; i< s.length();i++){
+
+        }
         return "";
     }
     public static void main(String[] args){
